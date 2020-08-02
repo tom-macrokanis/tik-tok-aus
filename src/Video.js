ebaea7fd73b6dc9b1b06d23709b14f7c7a5e8cdf
Tom Macrokanis <macroktg@gmail.com>
2020-08-02 19:12:10 +1000

    jjj

--- src/Video.js
+++ src/Video.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import './Video.css'
 
 function Video() {
+    const [play, /*setplay*/] = useState(false);
+    const videoRef = useRef(null);
+    const onVideoPress = () => {
+        if (play) {
+            videoRef.current.pause();
+        } else {
+        videoRef.current.play();
+        }
+    };
+
     return  (
     <div className="video">
-        <video className="video__player" 
+        <video className="video__player"
         loop
+        onClick={onVideoPress}
+        ref={videoRef}
         src="https://www.tiktok.com/@abrameng/video/6852099259865615622">
-
         </video>
         {/* <VideoFooter/> */}
-        {/* <VideoSidebar/>*/ }
+        {/* <VideoSidebar/>*/}
     </div>
     );
     
