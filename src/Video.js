c6bd23f7bd47b50aed03cbb5260e9518c9ab5793
Tom Macrokanis <macroktg@gmail.com>
2020-08-30 18:06:28 +1000

    update

--- src/Video.js
+++ src/Video.js
@@ -1,26 +1,30 @@
 import React, {useRef, useState} from "react";
-import './Video.css'
+import VideoFooter from './VideoFooter.js';
+import './Video.css';
 
 function Video() {
-    const [play, /*setplay*/] = useState(false);
+    const [playing, setplay] = useState(false);
     const videoRef = useRef(null);
     const onVideoPress = () => {
-        if (play) {
+        if (playing) {
             videoRef.current.pause();
+            setplay(false);
         } else {
-        videoRef.current.play();
+            videoRef.current.play();
+            setplay(true);
         }
     };
 
     return  (
     <div className="video">
-        <video className="video__player"
+        <video 
+        className="video__player"
         loop
         onClick={onVideoPress}
         ref={videoRef}
-        src="https://www.tiktok.com/@abrameng/video/6852099259865615622">
+        src="https://v16m.tiktokcdn.com/57053a57570daffee63f4aad8859cb7b/5f293a9b/video/tos/useast2a/tos-useast2a-ve-0068c004/daefa85f76b44411a2c8db9b3ec00321/?a=1233&br=2440&bt=1220&cr=0&cs=0&dr=0&ds=3&er=&l=202008021038180101902090882BB7E224&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajkzanJmeXhqdTMzMzczM0ApOTYzZDNkNWVnN2llaDlnZGdmb2Jra2A0LWFfLS0yMTZzcy0uMi5jXi8uLi8zNGMyLjI6Yw%3D%3D&vl=&vr=">
         </video>
-        {/* <VideoFooter/> */}
+        <VideoFooter />
         {/* <VideoSidebar/>*/}
     </div>
     );
---
+++ src/VideoFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoFooter.css';
+
+function VideoFooter() {
+    return (
+    <div className="videoFooter">
+        <h2>This is a footer</h2>
+    </div>
+    );
+}
+
+export default VideoFooter
